988948de0572f270ab1561ca9fd5340d60d16719
zainabjameel22 <62748206+zainabjameel22@users.noreply.github.com>
2021-06-21 15:09:41 +0300

    Add files via upload

---
+++ glorious_inari_blad1 (1).ino
@@ -0,0 +1,48 @@
+
+#include <Servo.h>
+Servo myServo1;
+Servo myServo2;
+Servo myServo3;
+Servo myServo4;
+Servo myServo5;
+
+int pos=0;
+void setup()
+{
+ myServo1.attach(9);
+   myServo2.attach(10);
+   myServo3.attach(11);
+  myServo4.attach(12);
+  myServo5.attach(13);
+}
+
+void loop()
+{
+  for(pos=0;pos<=90;pos++)
+{
+ myServo1.write(pos);
+  delay(10);
+  myServo2.write(pos);
+  delay(10);
+  myServo3.write(pos);
+  delay(10);
+  myServo4.write(pos);
+  delay(10);
+  myServo5.write(pos);
+  delay(10);
+  }
+for(pos=90;pos>=0;pos--)
+{
+  myServo1.write(pos);
+  delay(10);
+  myServo2.write(pos);
+  delay(10);
+  myServo3.write(pos);
+  delay(10);
+  myServo4.write(pos);
+  delay(10);
+  myServo5.write(pos);
+  delay(10);
+}
+  
+}
